0999d7a922491dc61048a0ca13b74242ddfa2cb9
Soumya Kushwaha <87495134+Soumya-Kushwaha@users.noreply.github.com>
2022-02-02 10:16:33 +0530

    Time: 24 ms (10.73%), Space: 5.9 MB (79.74%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n==0)
+            return 0;
+        
+        else if (n==1)
+            return 1;
+        
+        else if (n==2)
+            return 1;
+        
+        else if (n > 2)
+            return ((fib (n-1)) + (fib (n-2)) );
+        
+        else
+            return 0;
+    
+        
+    }
+};
