114eab893685a79b11f5844c2c9c2134954b63e9
Soumya Kushwaha <87495134+Soumya-Kushwaha@users.noreply.github.com>
2022-02-16 13:01:41 +0530

    Time: 46 ms (12.54%), Space: 17.8 MB (93.71%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
@@ -16,5 +16,32 @@ public:
                 return i;
         }
         return 0;
+    }
+};
+*/
+
+class Solution{
+public:
+    int missingNumber(vector<int>&nums){
+        int n = nums.size();
+        if (n==1){
+            if (nums[0] == 0)
+                return 1;
+            else return 0;
+        }
+        
+        if (n>1){
+            sort(nums.begin(), nums.end());
+            if (nums[0] != 0)
+                return 0;
+            int i=1;
+            
+            while (i<=n-1){
+                if (nums[i] - nums[i-1] == 2)
+                    return nums[i-1]+1;
+                i++;
+            }
+        }
+        return nums[n-1] + 1;
     }
 };
