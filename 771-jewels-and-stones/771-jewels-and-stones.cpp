8e870bc043e39ed2bcd36edd0ae6fddfeb447a74
Soumya Kushwaha <kushsoumya30@gmail.com>
2022-10-07 22:56:48 +0530

    Time: 2 ms (69.97%), Space: 6 MB (95.29%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    
+    int numJewelsInStones(string jewels, string stones) {
+        int num = 0;
+        for (int i = 0; i < jewels.size(); i++){
+            num += count(stones.begin(), stones.end(), jewels[i]);
+        }
+        return num;
+    }
+};
+
+    
+/*
     int numJewelsInStones(string jewels, string stones) {
         int stone_size = stones.size();
         int jewel_size = jewels.size();
@@ -12,4 +24,4 @@ public:
         }
         return count;
     }
-};
+*/
