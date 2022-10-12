52e08b4e4a70ff81d430f8f9ce429b9edbbafb64
Soumya Kushwaha <kushsoumya30@gmail.com>
2022-10-12 15:07:04 +0530

    Time: 2220 ms (5.05%), Space: 5.9 MB (26.02%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -19,3 +19,15 @@ public:
         return count;
     }
 };
+
+/*
+int minMoves(int target, int maxDoubles) {
+        if(target==1)   return 0;
+        if(maxDoubles==0)
+            return target-1;
+        if(target%2)
+            return minMoves(target-1, maxDoubles)+1;
+        else
+            return minMoves(target/2, maxDoubles-1)+1;
+    }
+*/
