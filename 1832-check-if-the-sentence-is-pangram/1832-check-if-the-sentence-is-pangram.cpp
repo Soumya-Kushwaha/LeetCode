0508ce50d6705019007b6491ec3bf8fbcbf76218
Soumya Kushwaha <kushsoumya30@gmail.com>
2022-10-17 23:32:38 +0530

    Time: 4 ms (65.37%), Space: 7.1 MB (6.60%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        
+        unordered_set<char> st (sentence.begin(), sentence.end());
+        return st.size() == 26;  
+    }
+};
