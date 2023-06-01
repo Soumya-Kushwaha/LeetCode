15e14a327f509a84ff773ba68ccbfa286646e2c6
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-06-01 21:02:39 +0530

    Time: 3 ms (63.24%), Space: 5.9 MB (93.25%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1)
+            return 1;
+        
+        int left = 1;
+        int right = x/2;
+        
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            
+            if (mid == x / mid)
+                return mid;
+            
+            else if (mid < x / mid)
+                left = mid + 1;
+            
+            else right = mid - 1;
+        }
+        
+        return right;        
+    }
+};
