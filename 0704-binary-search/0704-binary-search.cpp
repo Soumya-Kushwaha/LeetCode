51a23587f63a27a6000cc57c057a30472d53aa24
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-06-04 09:21:35 +0530

    Time: 44 ms (39.87%), Space: 27.5 MB (50.71%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
         int left = 0;
         int right = nums.size() - 1;
         
-        while (left <= right){
+        while(left <= right){
             int mid = left + (right - left)/2;
             
             if (nums[mid] == target)
@@ -14,6 +13,6 @@ public:
                 right = mid - 1;
             else left = mid + 1;
         }
-        return -1;
+        return -1;        
     }
 };
