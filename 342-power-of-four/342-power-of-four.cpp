f6c73898e5c4d8619d8e70c920e97b3ef930784f
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-06-27 10:48:38 +0530

    Delete 342-power-of-four directory

--- 342-power-of-four/342-power-of-four.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    bool isPowerOfFour(int n) {
-        if (n <=0)
-            return false;
-        else
-            return ((n & (n-1)) == 0 && ((n-1) % 3 == 0));
-    }
-};
