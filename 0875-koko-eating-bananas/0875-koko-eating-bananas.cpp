bdec9ae93b318a33b837b97286675e20251a71ca
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-07-02 00:20:56 +0530

    Time: 56 ms (53.71%), Space: 18.8 MB (79.58%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        int l = 1, r = 1000000000;
-        while (l < r) {
-            int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > H)
-                l = m + 1;
-            else
-                r = m;
+
+long long reqTime(vector<int> & piles, int mid){
+    long long  time = 0;
+
+    for (int i=0; i<piles.size(); i++)
+        time += ceil((double)piles[i]/(double)mid);
+    
+    return time;
+}
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxElement = INT_MIN;
+        int n = piles.size();
+
+        for (int i=0; i<n; i++)
+            maxElement = max(maxElement, piles[i]);
+
+        int low = 1, high = maxElement;
+
+        while (low <= high){
+            int mid = low + (high - low)/2;
+
+            if (reqTime(piles, mid) <= h)
+                high = mid - 1;
+            else 
+                low = mid + 1;
         }
-        return l;
+        return low;
     }
 };
