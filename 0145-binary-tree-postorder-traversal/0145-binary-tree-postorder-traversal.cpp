29c63c28bc6fe3706c8375d8adb2a96f477aefaf
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-07-08 23:40:38 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (79.90%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,6 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//    ITERATIVE SOLUTION - 2 STACK
+class Solution{
+public:
+    vector<int> postorderTraversal(TreeNode* root){
+        vector<int> nodes;
+        if (root == NULL)
+            return nodes;
+
+        stack <TreeNode*> st1, st2;
+        TreeNode* node = root;
+        st1.push(node);
+
+        while (!st1.empty()){
+            node = st1.top();
+            st1.pop();
+            if (node->left != NULL)
+                st1.push(node->left);
+            if (node->right != NULL)
+                st1.push(node->right);
+            st2.push(node);
+        }
+         while (!st2.empty()){
+            node = st2.top();
+            st2.pop();
+            nodes.push_back(node->val);
+        }
+        return nodes;             
+    }
+};
+
+
+//    RECURSIVE SOLUTION
+ 
+ /*
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -26,3 +61,4 @@ private:
         nodes.push_back(root->val);
     }
 };
+*/
