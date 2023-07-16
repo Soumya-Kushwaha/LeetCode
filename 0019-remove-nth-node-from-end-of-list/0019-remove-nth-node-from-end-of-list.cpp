82014f4d266318317511c44ae24c6e61e12f1e6b
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-07-16 10:50:07 +0530

    Time: 3 ms (81.41%), Space: 10.7 MB (10.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,28 +8,71 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-        if (!head)
+        ListNode *temp = new ListNode();
+        temp -> next = head;
+        ListNode * fast = temp;
+        ListNode * slow = temp;
+        int count = 0;
+        while (count < n){
+            fast = fast -> next;
+            count++;
+        }
+        while(fast -> next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow -> next = slow->next->next;
+        return temp->next;
+    }
+};
+
+/*  TC - O(2N)
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (head == NULL)
             return NULL;
         
-        ListNode *slow = head;
-        ListNode *fast = head;
+        ListNode *temp = head;
+        int count = 1;
         
-        for (int i=0; i < n; i++)
-            fast = fast -> next;
-        
-        if (!fast)
-            return head -> next;
-        
-        while (fast->next != NULL){
-            fast = fast -> next;
-            slow = slow -> next;
+        // length of linked list
+        while(temp->next != NULL){
+            count++;
+            temp = temp->next;
         }
         
-        slow -> next = slow -> next -> next;
+        // position to remove node
+        int position = count - n + 1;
+
+        // remove first node
+        if (position == 1){
+            ListNode *temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+            return head;
+        }
+        // remove node
+        else{
+            ListNode *curr = head;
+            ListNode *prev = NULL;
+            int count = 1;
+            while(count < position){
+                prev = curr;
+                curr = curr->next;
+                count++;
+            }
+            prev->next = curr->next;
+            curr->next = NULL;
+            delete curr;
+        }
         return head;
     }
 };
+*/
