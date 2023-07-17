203a89c8c8c395b152b4eaa78fda1f85f6d9d3e6
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-07-17 23:23:16 +0530

    Time: 55 ms (45.61%), Space: 14.7 MB (31.25%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -5,19 +5,93 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
- 
- class Solution {
-public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        
-    }
-};
  */
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (headA == NULL || headB == NULL)
-            return NULL;
+        ListNode *tempA = headA, *tempB = headB;
+        while(tempA != tempB){
+            if(tempA == NULL)
+                tempA = headB;
+            else tempA = tempA ->next;
+            
+            if(tempB == NULL)
+                tempB = headA;
+            else tempB = tempB ->next;
+        }
+        return tempA;
+    }
+};
+
+
+/* DIfference in Length:
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        ListNode *tempA = headA, *tempB = headB;
+        int lenA = 0, lenB = 0;
+        while (tempA != NULL){
+            lenA++;
+            tempA = tempA -> next;
+        }
+        while (tempB != NULL){
+            lenB++;
+            tempB = tempB -> next;
+        }
+        
+        // to sync pointers
+        if (lenA > lenB){
+            while(lenA != lenB){
+                headA = headA -> next;
+                lenA--;
+            }
+        }
+        else{
+            while(lenA != lenB){
+                headB = headB -> next;
+                lenB--;
+            }
+        }
+        while (headA != NULL){
+            if (headA == headB)
+                return headA;
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
+
+
+// Hashing:
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+               
+        unordered_set<ListNode*> st;
+        while (headA != NULL){
+            st.insert(headA);
+            headA = headA->next;
+        }
+        while (headB != NULL){
+            if (st.find(headB) != st.end())
+                return headB;
+            else headB = headB->next;
+        }
+        return NULL;
+    }
+};
+
+
+// Brute Force Approach:
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
         while(headA != NULL){
             ListNode *temp = headB;
@@ -31,3 +105,4 @@ public:
         return NULL;
     }
 };
+*/
