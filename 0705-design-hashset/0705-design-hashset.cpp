468e514977f56cf627900cca888f29f8dd8efbdf
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-08-01 11:29:12 +0530

    Time: 80 ms (95.40%), Space: 41 MB (80.05%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,5 +1,47 @@
 class MyHashSet {
 public:
+    vector<list<int>> m;
+    int siz;
+    
+    MyHashSet() {
+        siz = 100;
+        m.resize(siz);
+    }
+    
+    int hash(int key){
+        return (key % siz);
+    }
+    
+    list<int> :: iterator search(int key){
+        int i = hash(key);
+        return find(m[i].begin(), m[i].end(), key);
+    }
+    
+    void add(int key) {
+        if (contains(key))
+            return;
+        int i = hash(key);
+        m[i].push_back(key);
+    }
+    
+    void remove(int key) {
+        if (!contains(key))
+            return;
+        int i = hash(key);
+        m[i].erase(search(key));
+    }
+    
+    bool contains(int key) {
+        int i = hash(key);
+        if (search(key) != m[i].end())
+            return true;
+        else return false;
+    }
+};
+
+/*
+class MyHashSet {
+public:
     vector<int> m;
     MyHashSet() {
         int siz = 1e6 + 1;
@@ -18,33 +60,11 @@ public:
         return m[key];
     }
 };
-
+*/
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet* obj = new MyHashSet();
  * obj->add(key);
  * obj->remove(key);
  * bool param_3 = obj->contains(key);
- 
- class MyHashSet {
-public:
-    vector<int> m;
-    MyHashSet() {
-        int siz = 1e6 + 1;
-        m.resize(z);
-    }
-    
-    void add(int key) {
-        
-    }
-    
-    void remove(int key) {
-        
-    }
-    
-    bool contains(int key) {
-        
-    }
-};
-
-*/
+ */
