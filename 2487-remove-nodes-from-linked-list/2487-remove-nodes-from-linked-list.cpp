fd8bfcd49e019cad6a99b832fa41b53308689270
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-08-30 01:15:31 +0530

    Time: 340 ms (68.31%), Space: 160.9 MB (61.86%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* reverse(ListNode* head){
+        if (head -> next == NULL)
+            return head;
+        
+        ListNode *reverseHead = reverse(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return reverseHead;
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL)
+            return NULL;
+        return reverse(head);
+    }
+    
+    ListNode* removeNodes(ListNode* head) {
+        ListNode* curr = reverse(head);
+        ListNode* temp = curr;
+        int x = temp -> val;
+        while (temp -> next != NULL){
+            if (x > temp->next->val){
+                temp -> next = temp -> next -> next;
+            }
+            else{
+                x = temp -> next -> val;
+                temp = temp -> next;
+            }
+        }
+        temp = reverse(curr);
+        return temp;
+    }
+};
