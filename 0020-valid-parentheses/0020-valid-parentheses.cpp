b56291f59547480d7edb99cad7f66be7a922c96c
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-09-14 18:16:41 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (12.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {    
+        stack<char> st;
+        
+        for (int i=0; i<s.size(); i++){
+            if (st.empty())
+                st.push(s[i]);
+            
+            else if (st.top() == '(' && s[i] == ')' ||
+                st.top() == '[' && s[i] == ']' ||
+                st.top() == '{' && s[i] == '}')
+                st.pop();
+            
+            else st.push(s[i]);
+        }
+        return (st.empty());
+    }
+};
