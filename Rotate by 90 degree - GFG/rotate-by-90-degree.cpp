c54fda3c56e6f1b272b7f3add74eebea6e327ce7
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-10-01 23:36:30 +0530

    Added solution - LeetHub

--- Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.cpp
@@ -1,52 +1,55 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h> 
-using namespace std; 
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+void rotate (vector<vector<int> >& matrix);
+
 
 // } Driver Code Ends
+//User function template for C++
 
-class Solution
-{   
-    public:
-    //Function to rotate matrix anticlockwise by 90 degrees.
-    void rotateby90(vector<vector<int> >& matrix, int n) 
-    { 
-        for (int i=0; i<n; i++){
-            for (int j=i+1; j<n; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
+/* matrix : given input matrix, you are require 
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& matrix)
+{
+    int n = matrix.size();
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            swap(matrix[i][j], matrix[j][i]);
         }
-        reverse(matrix.begin(), matrix.end());
-    } 
-};
+    }
+    reverse(matrix.begin(), matrix.end());
+}
 
 
 //{ Driver Code Starts.
-int main() {
+
+int main()
+{
     int t;
-    cin>>t;
-    
-    while(t--) 
+    cin>>t; 
+    while(t--)
     {
         int n;
-        cin>>n;
-        vector<vector<int> > matrix(n); 
-
+        cin>>n; 
+        vector<vector<int> > matrix(n);
         for(int i=0; i<n; i++)
         {
-            matrix[i].assign(n, 0);
-            for( int j=0; j<n; j++)
-            {
-                cin>>matrix[i][j];
-            }
+            matrix[i].resize(n);
+            for(int j=0; j<n; j++)
+                cin>>matrix[i][j]; 
         }
-
-        Solution ob;
-        ob.rotateby90(matrix, n);
+        rotate(matrix);
         for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
+        {
+            for(int j=0; j<n; j++)
                 cout<<matrix[i][j]<<" ";
-        cout<<endl;
+            cout<<"\n";
+        }
     }
     return 0;
 }
+
+
 // } Driver Code Ends
