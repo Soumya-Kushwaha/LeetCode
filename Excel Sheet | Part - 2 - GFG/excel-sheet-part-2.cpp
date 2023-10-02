cecad6558c52e673e096043f003c172f766be02e
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-10-03 01:13:31 +0530

    Added solution - LeetHub

---
+++ Excel Sheet | Part - 2 - GFG/excel-sheet-part-2.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    int ExcelColumnNumber(string s)
+    {
+        int result = 0;
+        
+        for (int i=0; i<s.size(); i++){
+            int num = s[i] - 'A' + 1;
+            result = result * 26 + num;
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.ExcelColumnNumber(s)<<endl;
+    }
+}
+// } Driver Code Ends
