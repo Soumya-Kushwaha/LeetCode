e0d252537b344b0d0ba907823836f51ea031f1df
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-10-11 18:48:08 +0530

    Time: 247 ms (63.34%), Space: 88 MB (52.99%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -4,14 +4,17 @@ public:
         int n = people.size();
         vector<int> start, end;
         
+        // segregate start & end values in separate vectors
         for (auto it: flowers){
             start.push_back(it[0]);
             end.push_back(it[1]);
         }
         
+        // sort the values to apply binary search
         sort(start.begin(), start.end());
         sort(end.begin(), end.end());
         
+        // blooming flowers - withered flowers
         vector<int> bloom(n, -1);
         for (int i=0; i<n; i++){
             int bloomStart = upper_bound(start.begin(), start.end(), people[i]) - start.begin();
