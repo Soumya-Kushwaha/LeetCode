77850ce88cabf3c1e0c3a8c72bd117a4e4436487
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-10-18 00:56:18 +0530

    Time: 21 ms (28.81%), Space: 19.6 MB (36.78%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int x = -1;
-        for(int i=0; i<nums.size(); i++)
-            if (nums[i] == 0){
-                x = i;
-                break;
-            }
-        
-        if (x == -1)
-            return;
-        
-        for (int i=x+1; i<nums.size(); i++){
+        int count = 0;
+        for (int i=0; i<nums.size(); i++){
             if (nums[i] != 0){
-                swap(nums[i], nums[x]);
-                x++;
+                nums[count] = nums[i];
+                count++;
             }
         }
-        
+        for (int i=count; i<nums.size(); i++)
+            nums[i] = 0;
     }
 };
