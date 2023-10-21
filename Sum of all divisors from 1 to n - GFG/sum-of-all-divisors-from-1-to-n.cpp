b1276f04ae7665303c07a916efd3759e1cfb39c7
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-10-21 20:24:17 +0530

    Added solution - LeetHub

---
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        long long summ = 0;
+        for (int i=1; i<=N; i++){
+            summ += (N/i)*i;
+        }
+        return summ;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
