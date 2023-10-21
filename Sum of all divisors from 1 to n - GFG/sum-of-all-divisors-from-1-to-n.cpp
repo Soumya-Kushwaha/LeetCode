be4ed44f4ec43e18e87dee7d4870443fc597b12c
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-10-21 20:27:09 +0530

    Added solution - LeetHub

--- Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
@@ -9,13 +9,24 @@ class Solution
 public:
     long long sumOfDivisors(int N)
     {
+        // Initialize the sum to 0
         long long summ = 0;
+        
+        // Iterate from 1 to N
         for (int i=1; i<=N; i++){
+            
+            // Add the contribution of each divisor to the sum
             summ += (N/i)*i;
         }
+        
         return summ;
     }
 };
+
+// 1 occurs how many times? N -> So N/i 
+// what is it product finally? (N/i) * i 
+
+// Repeat for all numbers & shoot!
 
 //{ Driver Code Starts.
 int main()
