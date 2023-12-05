8117ea83553b466b1c6c614f716e38297023fb0d
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-12-06 00:29:42 +0530

    Time: 65 ms (97.81%), Space: 68.8 MB (83.74%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        stack<int> st;
+        
         int n = arr.size();
-        
-        st.push(arr[n-1]);
-        arr[n-1] = -1;
-        
+        vector<int> ans(n);
+        int maxi = arr[n-1];
+        ans[n-1] = -1;
+
         for (int i=n-2; i>=0; i--){
-           //if (!st.empty()){
-               if (arr[i] > st.top()){
-                   int temp = st.top();
-                   st.push(arr[i]); 
-                   arr[i] = temp;
-                     
-               }
-               else{
-                   int temp = st.top();
-                   arr[i] = temp;
-               }
-          // }
+            ans[i] = maxi;
+            maxi = max(maxi, arr[i]);
         }
-        return arr;
+
+        return ans;
     }
 };
+     
+
+// class Solution {
+// public:
+//     vector<int> replaceElements(vector<int>& arr) {
+//         stack<int> st;
+//         int n = arr.size();
+
+//         st.push(arr[n-1]);
+//         arr[n-1] = -1;
+
+//         for (int i=n-2; i>=0; i--){
+//                if (arr[i] > st.top()){
+//                    int temp = st.top();
+//                    st.push(arr[i]); 
+//                    arr[i] = temp;
+
+//                }
+//                else{
+//                    int temp = st.top();
+//                    arr[i] = temp;
+//                }
+//         }
+//         return arr;
+//     }
+// };
