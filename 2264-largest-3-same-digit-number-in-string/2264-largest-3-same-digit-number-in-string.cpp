a2979cc525fa707dbf1498d83846794426eb5bdf
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-12-06 01:02:28 +0530

    Time: 2 ms (53.53%), Space: 6.8 MB (59.41%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -5,13 +5,15 @@ public:
         int ans = -1;
         for (int i=0; i<n-2; i++){
             if ((num[i] == num[i+1]) && (num[i] == num[i+2])){
-                int temp = (num[i]-'0');
-                ans = max(temp, ans);
+                //int temp = (num[i]-'0');
+                ans = max(num[i]-'0', ans);
             }
         }
         if (ans == -1)
             return "";
-        string res = to_string(ans) + to_string(ans) + to_string(ans);
-        return res;
+        return std::string(3, '0' + ans);
+        
+//        string res = to_string(ans) + to_string(ans) + to_string(ans);
+//        return res;
     }
 };
