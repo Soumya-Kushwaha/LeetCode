989183c1c293228fb70f73516e352e4a53a7f414
Soumya Kushwaha <kushsoumya30@gmail.com>
2024-03-14 23:58:48 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (74.39%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,19 +1,25 @@
-class Solution {
+class Solution{
 public:
-    int pivotInteger(int n) {
-        int leftBound = 1, rightBound =  n;
-        int fullSum = (n * (n + 1))/2;
-        while(leftBound <= rightBound){
-            int midNum = leftBound + (rightBound - leftBound) / 2;
-            int firstHlfSum = (midNum * (midNum + 1))/2;
-            int secondHlfSum = fullSum - firstHlfSum + midNum;
-            if(firstHlfSum == secondHlfSum)return midNum;
-            else if(firstHlfSum < secondHlfSum){
-                leftBound = midNum + 1;
-            }else{
-                rightBound = midNum - 1;
-            }
+    int pivotInteger(int n){
+        int left = 1, right = n;
+        int summ = (n * (n + 1)) / 2;
+        
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            
+            int firstHalfSum = (mid * (mid + 1)) / 2;
+            int secondHalfSum = summ - firstHalfSum + mid;
+            
+            if (firstHalfSum == secondHalfSum)
+                return mid;
+            
+            else if (firstHalfSum > secondHalfSum)
+                right = mid - 1;
+            
+            else
+                left = mid + 1;
         }
+        
         return -1;
     }
 };
