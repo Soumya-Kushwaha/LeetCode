e3f24081f2aaa8462c6eebfeaeccd4504859aa74
Soumya Kushwaha <kushsoumya30@gmail.com>
2024-04-04 23:24:09 +0530

    Time: 3 ms (40.00%), Space: 7.4 MB (78.96%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     int maxDepth(string s) {
         
-        stack<char> st;
-        int ans = 0;
-        
-        for (int i=0; i<s.size(); i++)
+        int ans = 0, count = 0;        
+        for(int i=0; i<s.size(); i++)
         {
-            // push open parentheses to the stack
             if (s[i] == '(')
-                st.push(s[i]);
-            
-            // find the maximum number of open parentheses when a close parentheses is encountered
-            // and pop
+                count++;
             else if (s[i] == ')')
-            {
-                int n = st.size();
-                ans = max(n, ans);
-                
-                st.pop();
-            }
+                count--;
+            ans = max(ans, count);
         }
-        
         return ans;
     }
 };
